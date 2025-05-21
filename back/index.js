d2deb3df6b9ecc163ee980c28ceb3943ebc616d7
Rosines <r.da.silva.almeida@avanade.com>
2025-05-21 10:49:14 -0300

    projeto back: add arquivo enviroment para armazenamento password

--- back/index.js
+++ back/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { environment } = require('./enviroment');
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Umy159'  ,  
+  password: environment.DB_PASSWORD, 
   database: 'requisitos_db'
 });
 
